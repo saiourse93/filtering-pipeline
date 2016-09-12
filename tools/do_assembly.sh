bd675910e70093fe3008604249ff35c985cf98a6
Phelelani Mpangase <pmpangase@gmail.com>
2016-09-12 08:12:59 +0200

    Final assembly script updated

--- tools/do_assembly.sh
+++ tools/do_assembly.sh
@@ -2,14 +2,12 @@
 
 #PBS -N RNA_Assembly
 #PBS -q WitsLong
-#PBS -l walltime=100:00:00,mem=24G
+#PBS -l walltime=100:00:00,mem=24gb
 #PBS -l nodes=1:ppn=7
-#PBS -o 
-#PBS -e 
 
 #Folders needed
-WORK_DIR=
+WORK_DIR=/home/phelelani/scleroderma_analysis/jobs/assembly-pipeline/tools # Change to your working directory!
 
 cd $WORK_DIR
 
-Trinity --seqType fq --max_memory 24G --left $left_reads --right $right_reads --SS_lib_type RF --CPU 6
+Trinity --seqType fq --max_memory 24G --left CASE_01_ARM_001_filtered_R1.fastq --right CASE_01_ARM_001_filtered_R2.fastq --SS_lib_type RF --CPU 6
