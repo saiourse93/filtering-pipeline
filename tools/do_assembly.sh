78e026d80498ea8d9c232789c72b991c5fce7819
Adriot <adroit.vukosi@gmail.com>
2016-09-27 21:05:28 +0200

    put results on a single dir (RESULTS) on trinity_out_dir

--- tools/do_assembly.sh
+++ tools/do_assembly.sh
@@ -14,21 +14,26 @@ samples=sample_dirs
 
 while IFS= read -r file
 do
-        rm /$WORK_DIR/trinity_out_dir/Trinity.fasta
+        rm $WORK_DIR/trinity_out_dir/Trinity.fasta
 
-        if [ -s /$WORK_DIR/trinity_out_dir/$file/Trinity.fasta ]
+        if [ -s $WORK_DIR/trinity_out_dir/RESULTS/$file/Trinity.fasta ]
         then
-            rm /$WORK_DIR/trinity_out_dir/$file/Trinity.fasta
+            rm $WORK_DIR/trinity_out_dir/RESULTS/$file/Trinity.fasta
         fi
 
         /opt/exp_soft/bioinf/trinity/Trinity --seqType fq --max_memory 24G --left $DATA_DIR/$file/*filtered_R1.fastq \
         --right $DATA_DIR/$file/*filtered_R2.fastq --SS_lib_type RF --CPU 6
 
-        if [ ! -d /$WORK_DIR/trinity_out_dir/$file ]
+        if [ ! -d $WORK_DIR/trinity_out_dir/RESULTS ]
         then
-            mkdir /$WORK_DIR/trinity_out_dir/$file
+            mkdir $WORK_DIR/trinity_out_dir/RESULTS
         fi
 
-        cat /$WORK_DIR/trinity_out_dir/Trinity.fasta > /$WORK_DIR/trinity_out_dir/$file/Trinity.fasta
+        if [ ! -d $WORK_DIR/trinity_out_dir/RESULTS/$file ]
+        then
+            mkdir $WORK_DIR/trinity_out_dir/RESULTS/$file
+        fi
+
+        cat $WORK_DIR/trinity_out_dir/Trinity.fasta > $WORK_DIR/trinity_out_dir/RESULTS/$file/Trinity.fasta
 
 done <"$samples"
